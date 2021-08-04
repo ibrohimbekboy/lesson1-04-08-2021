fe62fbcfcd1057a0fd2812157b1ef73d1cd5e3e7
Student <student@go.alif.academy>
2021-08-04 20:36:27 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lesson1
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] args)
+        {
+            // Дз 1
+            double a = 16.80;
+            double b = 12.40;
+            double c = Math.Sqrt (a * b);
+            System.Console.WriteLine(c);
+
+
+            // Дз 2
+            double a1 = 1.40;
+            double b1 = -5.50;
+            double c1 = 0.60;
+            double d1 = (a1 - c1);
+            double e1 = (b1 - c1);
+            double f1 = (d1 + e1);
+
+            // Дз 3
+            double x1 = -6.20;
+            double y1 = 5.20;
+            double x2 = 2.10;
+            double y2 = 9.80;
+            System.Console.WriteLine(Math.Sqrt ((Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2))));
+            //Извиняюсь за то что не все задания выполнил, мой ноутбук на ремонте,
+            // а пришел я в алиф академию поздновато, спасибо!
+            
+        } 
+
+    }}
+
